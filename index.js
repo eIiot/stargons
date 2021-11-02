9d797fb956edbdcf41716b1c62a7077e3f3b14d0
Eliot Hertenstein <39346975+eIiot@users.noreply.github.com>
2021-11-01 20:07:47 -0700

    finished html page

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 //Define Variables
-var radius = 80;
-var point_size = 4;
-var center_x = 150;
-var center_y = 150;
+var radius = 80*2;
+var point_size = 4*2;
+var center_x = 200;
+var center_y = 200;
 var font_size = "20px";
 
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
-// function close that will close tab
-window.__defineGetter__("hampter", function() { alert("我叫李"); });
+// hello antoine
+window.__defineGetter__("hampter", function() {
+  alert("我叫李");
+  window.close(); 
+});
+
+// get sliders, and slider labels
+var numPointsSlider = document.getElementById("points");
+var numPointsLabel = document.getElementById("points-label");
+var nthPointsSlider = document.getElementById("nth");
+var nthPointsLabel = document.getElementById("nth-label");
+var biggerSliderButton = document.getElementById("bigger");
+var smallerSliderButton = document.getElementById("smaller");
 
 function drawCircle(){
     ctx.beginPath();
@@ -25,13 +36,21 @@ function drawPoint(angle,distance,label){
     ctx.arc(x, y, point_size, 0, 2 * Math.PI);
     ctx.fill();
 
-    ctx.font = font_size;
-    ctx.fillText(label,x + 10,y);
+    // ctx.font = font_size;
+    // ctx.fillText(label,x + 10,y);
 };
 
-drawCircle();
+function drawLine(point1,point2){
+    ctx.beginPath();
+    ctx.moveTo(point1.x,point1.y);
+    ctx.lineTo(point2.x,point2.y);
+    ctx.stroke();
+};
 
-function drawPoints(numPoints,connectEveryNth){
+function createStargon(numPoints,connectEveryNth){
+  numPoints = parseInt(numPoints);
+  connectEveryNth = parseInt(connectEveryNth);
+
   // clear canvas
   ctx.clearRect(0, 0, c.width, c.height);
 
@@ -42,6 +61,39 @@ function drawPoints(numPoints,connectEveryNth){
       drawPoint(angle * i,1,"P" + i);
   };
 
-  // draw a line between every point, and the nth point after it
+  // for each point, draw a line nth next point
+  for(var i = 0; i < numPoints; i++){
+      var point1 = {x:center_x + radius * Math.cos(-angle*i*Math.PI/180) * 1,y:center_y + radius * Math.sin(-angle*i*Math.PI/180) * 1};
+      var point2 = {x:center_x + radius * Math.cos(-angle*(i+connectEveryNth)*Math.PI/180) * 1,y:center_y + radius * Math.sin(-angle*(i+connectEveryNth)*Math.PI/180) * 1};
+      drawLine(point1,point2);
+  };
 
+  // for(var i = 0; i < numPoints; i++){
+  //     var point1 = {x:center_x + radius * Math.cos(-angle*i*Math.PI/180) * 1,y:center_y + radius * Math.sin(-angle*i*Math.PI/180) * 1};
+  //     var point2 = {x:center_x + radius * Math.cos(-angle*(i+connectEveryNth)*Math.PI/180) * 1,y:center_y + radius * Math.sin(-angle*(i+connectEveryNth)*Math.PI/180) * 1};
+  //     drawLine(point1,point2);
+  // };
+};
+
+createStargon(numPointsSlider.value,nthPointsSlider.value);
+
+// set labels to current values
+numPointsLabel.innerHTML = numPointsSlider.value;
+nthPointsLabel.innerHTML = nthPointsSlider.value;
+
+// on slider change, update the number of points && the number of points to connect
+numPointsSlider.oninput = function() {
+  numPointsLabel.innerHTML = this.value;
+  createStargon(this.value,nthPointsSlider.value);
+};
+
+nthPointsSlider.oninput = function() {
+  nthPointsLabel.innerHTML = this.value;
+  createStargon(numPointsSlider.value,this.value);
+};
+
+biggerSliderButton.onclick = function() {
+  // add 10 to max of both sliders
+  numPointsSlider.max = parseInt(numPointsSlider.max) + 10;
+  nthPointsSlider.max = parseInt(nthPointsSlider.max) + 10;
 };
